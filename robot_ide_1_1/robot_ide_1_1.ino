132541ce7908910d6b81010073e6bf5061039bc8
gitofthere <gnelsondirect@outlook.com>
2021-09-22 18:45:48 -0800

    integrate protothreads

--- robot_ide_1_1/robot_ide_1_1.ino
+++ robot_ide_1_1/robot_ide_1_1.ino
@@ -1,12 +1,14 @@
 #include <Arduino.h>
-#include <Ultrasonic.h> 
+#include <Ultrasonic.h> // lib link - https://github.com/JRodrigoTech/Ultrasonic-HC-SR04
+#include <pt.h>   // include protothread library - https://roboticsbackend.com/arduino-protothreads-tutorial/
 
-//Wifi Module uses 
+//Wifi Module uses x pins
 
+static struct pt pt1, pt2; // setup each protothread
 
 //US1 is in front
-//US2 is facing left
-//US3 is facing right
+//US2 is facing left, so turn right
+//US3 is facing right, so turn left
 Ultrasonic us1(A0,A1); // (Trig PIN,Echo PIN)
 Ultrasonic us2(A2,A3); // pinout, pinin
 Ultrasonic us3(A4,A5);
@@ -18,7 +20,12 @@ int m1 = 10;
 
 
 void setup() {    
-  Serial.begin(9600);  
+  Serial.begin(9600); 
+ 
+  PT_INIT(&pt1);  // initialise protothread vars
+  PT_INIT(&pt2);
+  
+  //Ultrasonic Sensors (analog pins)
   pinMode(A0, OUTPUT);
   pinMode(A1, INPUT);
   pinMode(A2, OUTPUT);
@@ -26,6 +33,7 @@ void setup() {
   pinMode(A4, OUTPUT);
   pinMode(A5, INPUT);
   
+  // motors (digital pins)
   pinMode(m1, OUTPUT);     
   pinMode(m2, OUTPUT);
   pinMode(m3, OUTPUT);
@@ -34,10 +42,7 @@ void setup() {
 
 void loop()
 {
-  if (digitalRead(m1) != 0 || digitalRead(m2) != 1 || digitalRead(m3) != 0 || digitalRead(m4) != 1) {
-  forw();
-  } else {}
-  Serial.print("US-1 " + us1.Ranging(CM)); // CM or INC
+  /*Serial.print("US-1 " + us1.Ranging(CM)); // CM or INC
   Serial.println(" cm" );
   delay(10);
   Serial.print("US-2 " + us2.Ranging(CM)); // CM or INC
@@ -45,21 +50,35 @@ void loop()
   delay(10);
   Serial.print("US-3 " + us3.Ranging(CM)); // CM or INC
   Serial.println(" cm" );
-  delay(10);
-  
+  delay(10);*/
+  prot1(&pt1);
+  prot2(&pt2);
+}
+
+static int prot1(struct pt *pt) {
+  PT_BEGIN(pt);
+  if (digitalRead(m1) != 0 || digitalRead(m2) != 1 || digitalRead(m3) != 0 || digitalRead(m4) != 1) {
+  forw();
+  }
+  PT_END(pt);
+}
+
+static int prot2(struct pt *pt) {
+  PT_BEGIN(pt);
   if (us1.Ranging(CM) <= 10) {
     Serial.print("WARNING");
     back();
-    delay(200);
+    delay(10);
   } else if (us2.Ranging(CM) <= 10) {
     Serial.print("WARNING");;
     rturn();
-    delay(200);
+    delay(10);
   } else if (us3.Ranging(CM) <= 10) {
     Serial.print("WARNING");
     lturn();
-    delay(200);
-  } else {}
+    delay(10);
+  }
+  PT_END(pt);
 }
 
 void forw() {
