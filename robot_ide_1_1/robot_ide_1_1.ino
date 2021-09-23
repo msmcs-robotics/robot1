d04c4b0cc58b4b6e73ff10780998355d06c6519e
Gatlin Nelson <88689368+gitofthere@users.noreply.github.com>
2021-09-22 18:59:37 -0800

    comment clarity

--- robot_ide_1_1/robot_ide_1_1.ino
+++ robot_ide_1_1/robot_ide_1_1.ino
@@ -2,27 +2,29 @@
 #include <Ultrasonic.h> // lib link - https://github.com/JRodrigoTech/Ultrasonic-HC-SR04
 #include <pt.h>   // include protothread library - https://roboticsbackend.com/arduino-protothreads-tutorial/
 
-//Wifi Module uses x pins
+//Wifi Module uses x pins, adding later
 
-static struct pt pt1, pt2; // setup each protothread
+// setup each protothread
+static struct pt pt1, pt2;
 
-//US1 is in front
-//US2 is facing left, so turn right
-//US3 is facing right, so turn left
+// Create ultrasonic sensor objects to use in logic
 Ultrasonic us1(A0,A1); // (Trig PIN,Echo PIN)
 Ultrasonic us2(A2,A3); // pinout, pinin
 Ultrasonic us3(A4,A5);
 
+// setup motor pin variables
 int m4 = 13;
 int m3 = 12;
 int m2 = 11;
 int m1 = 10;
 
-
+// start serial monitor if needed (debugging), and
+// set pinout
 void setup() {    
-  Serial.begin(9600); 
+  // Serial.begin(9600); 
  
-  PT_INIT(&pt1);  // initialise protothread vars
+  // initialise protothread vars
+  PT_INIT(&pt1);
   PT_INIT(&pt2);
   
   //Ultrasonic Sensors (analog pins)
@@ -40,8 +42,13 @@ void setup() {
   pinMode(m4, OUTPUT);
 }
 
+// the infinite loop to run on the arduino
 void loop()
 {
+  
+  // Serial monitoring is turned off to save on 
+  // performance, but will be useful for debugging if needed
+  
   /*Serial.print("US-1 " + us1.Ranging(CM)); // CM or INC
   Serial.println(" cm" );
   delay(10);
@@ -51,10 +58,17 @@ void loop()
   Serial.print("US-3 " + us3.Ranging(CM)); // CM or INC
   Serial.println(" cm" );
   delay(10);*/
+  
+  // schedule threads indefinitely
   prot1(&pt1);
   prot2(&pt2);
 }
 
+
+/* This thread determines if the robot is
+   setup to move forward, if not, then start
+   moving the robot forward 
+*/
 static int prot1(struct pt *pt) {
   PT_BEGIN(pt);
   if (digitalRead(m1) != 0 || digitalRead(m2) != 1 || digitalRead(m3) != 0 || digitalRead(m4) != 1) {
@@ -63,6 +77,19 @@ static int prot1(struct pt *pt) {
   PT_END(pt);
 }
 
+/* This thread gathers and interprets data
+   from the ultrasonic sensors and makes
+   decisions for movement based on that data.
+   
+   Specifically, if an object is measured to 
+   be at or less than 10cm away, then the robot
+   will react in accordance to the logic below.
+   
+   SideNote: 
+   us1 is in front
+   us2 is facing left, so turn right
+   us3 is facing right, so turn left
+*/
 static int prot2(struct pt *pt) {
   PT_BEGIN(pt);
   if (us1.Ranging(CM) <= 10) {
@@ -81,13 +108,15 @@ static int prot2(struct pt *pt) {
   PT_END(pt);
 }
 
+  // Allocate voltage to motor pins so that
+  // the robot moves forward
 void forw() {
   digitalWrite(m1, LOW);// right wheel forward
   digitalWrite(m2, HIGH);//
   digitalWrite(m3, LOW);// left wheel forward
   digitalWrite(m4, HIGH);//
 }
-
+  // Allocate voltage to move backwards
 void back() {
   digitalWrite(m3, HIGH);// left wheel back
   digitalWrite(m4, LOW);//
@@ -95,14 +124,14 @@ void back() {
   digitalWrite(m2, LOW);//
 
 }
-
+  // Turn right
 void rturn() {
   digitalWrite(m1, HIGH);// right wheel back ward
   digitalWrite(m2, LOW);//
   digitalWrite(m3, LOW);// left wheel forward
   digitalWrite(m4, HIGH);//
 }
-
+  // Turn left
 void lturn() {
   digitalWrite(m3, HIGH);// left wheel back
   digitalWrite(m4, LOW);//
