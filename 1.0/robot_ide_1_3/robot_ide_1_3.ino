d9f626ef8d74a012fa8b95761ba5c066c1ff99bb
gitofthere <gnelsondirect@outlook.com>
2021-09-29 10:56:05 -0800

    PWM working

--- 1.0/robot_ide_1_3/robot_ide_1_3.ino
+++ 1.0/robot_ide_1_3/robot_ide_1_3.ino
@@ -1,14 +1,12 @@
 #include <Arduino.h>
 #include <Ultrasonic.h> // lib link - https://github.com/JRodrigoTech/Ultrasonic-HC-SR04
 #include <pt.h>   // include protothread library - https://roboticsbackend.com/arduino-protothreads-tutorial/
-#include <SD.h> // Defualt SD Card lib
-#include <SPI.h> // Req for SD Card
 
 // setup motor pin variables
-int m1 = 2;
-int m2 = 3;
-int m3 = 4;
-int m4 = 7;
+int m1 = 3;
+int m2 = 7;
+int m3 = 8;
+int m4 = 9;
 
 int p1 = 5;
 int p2 = 6;
@@ -21,15 +19,13 @@ boolean d;
 int pa;
 int pb; // ints will be PWM
 
+// setup each protothread
+static struct pt pt1;
 //Wifi Module uses x pins, adding later
 
 // Open a new file to log data on SD card
-File datalog = SD.open("datalog.txt", FILE_WRITE);
 // close file if needed 
 //datalog.close();
-
-// setup each protothread
-static struct pt pt1;
 
 // Create ultrasonic sensor objects to use in logic
 Ultrasonic us1(A0,A1); // (Trig PIN,Echo PIN)
@@ -40,14 +36,7 @@ Ultrasonic us3(A4,A5);
 // set pinout
 void setup() {    
   Serial.begin(9600); 
- 
-  // init SD Card
-  if (!SD.begin(10)) {
-    Serial.println("initialization failed!");
-    while (1);
-  }
-  
-  // initialise protothread vars
+
   PT_INIT(&pt1);
   
   //Ultrasonic Sensors (analog pins)
@@ -65,60 +54,49 @@ void setup() {
   pinMode(m4, OUTPUT);
   pinMode(p1, OUTPUT);
   pinMode(p2, OUTPUT);
-  Serial.print("Initializing SD card...");
-  if (!SD.begin(10)) {
-    Serial.println("initialization failed!");
-    while (1);
-  }
 }
 
-// the infinite loop to run on the arduino
-void loop()
-{
-  // schedule threads indefinitely
-  prot1(&pt1);
-}
 
 static int prot1(struct pt *pt) {
   PT_BEGIN(pt);
   if (us1.Ranging(CM) <= 10) {
     Serial.print("WARNING, going back");
-    a == HIGH;
-    b == LOW;
-    c == HIGH;
-    d == LOW;
-    pa == 255;
-    pb == 255;
+    a = HIGH;
+    b = LOW;
+    c = HIGH;
+    d = LOW;
+    pa = 255;
+    pb = 255;
     dm(a, b, c, d, pa, pb);
     delay(10);
   } else if (us2.Ranging(CM) <= 10) {
     Serial.print("WARNING, going right");;
-    a == HIGH;
-    b == LOW;
-    c == LOW;
-    d == HIGH;
-    pa == 255;
-    pb == 255;
+    a = HIGH;
+    b = LOW;
+    c = LOW;
+    d = HIGH;
+    pa = 255;
+    pb = 255;
     dm(a, b, c, d, pa, pb);
     delay(10);
   } else if (us3.Ranging(CM) <= 10) {
     Serial.print("WARNING, going left");
-    a == LOW;
-    b == HIGH;
-    c == HIGH;
-    d == LOW;
-    pa == 255;
-    pb == 255;
+    a = LOW;
+    b = HIGH;
+    c = HIGH;
+    d = LOW;
+    pa = 255;
+    pb = 255;
     dm(a, b, c, d, pa, pb);
     delay(10);
   } else {
     Serial.print("Moving...");
-    a == LOW;
-    b == HIGH;
-    c == LOW;
-    d == HIGH;
-    pa == 255;
-    pb == 255;
+    a = LOW;
+    b = HIGH;
+    c = LOW;
+    d = HIGH;
+    pa = 255;
+    pb = 255;
     dm(a, b, c, d, pa, pb);
   }
   PT_END(pt);
@@ -138,4 +116,7 @@ void dm(boolean a, boolean b, boolean c, boolean d, int pa, int pb) {
     datalog.println("Bools: "+"\nm1 - "+a+"\nm2 - "+b+"\nm3 - "+c+"\nm4 - "+d);
     datalog.println("PWM: "+"\nMotorA - "+pa+"\nMotorB - "+pb);
   }*/
+}
+void loop() {
+  prot1(&pt1);
 }
