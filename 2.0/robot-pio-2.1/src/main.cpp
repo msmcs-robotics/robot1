cbd79f54e66e8cbb040b7aaa246d91e206aae5c4
gitofthere <gnelsondirect@outlook.com>
2021-09-29 16:02:54 -0800

    reorg

--- 2.0/robot-pio-2.1/src/main.cpp
+++ 2.0/robot-pio-2.1/src/main.cpp
@@ -1,306 +1,10 @@
-// Nueral Network Reference: http://robotics.hobbizine.com/arduinoann.html
-
-/******************************************************************
- * ArduinoANN - An artificial neural network for the Arduino
- * All basic settings can be controlled via the Network Configuration
- * section.
- * See robotics.hobbizine.com/arduinoann.html for details.
- ******************************************************************/
-
-#include <math.h>
-
-/******************************************************************
- * Network Configuration - customized per network 
- ******************************************************************/
-
-const int PatternCount = 10;
-const int InputNodes = 7;
-const int HiddenNodes = 8;
-const int OutputNodes = 4;
-const float LearningRate = 0.3;
-const float Momentum = 0.9;
-const float InitialWeightMax = 0.5;
-const float Success = 0.0004;
-
-const byte Input[PatternCount][InputNodes] = {
-  { 1, 1, 1, 1, 1, 1, 0 },  // 0
-  { 0, 1, 1, 0, 0, 0, 0 },  // 1
-  { 1, 1, 0, 1, 1, 0, 1 },  // 2
-  { 1, 1, 1, 1, 0, 0, 1 },  // 3
-  { 0, 1, 1, 0, 0, 1, 1 },  // 4
-  { 1, 0, 1, 1, 0, 1, 1 },  // 5
-  { 0, 0, 1, 1, 1, 1, 1 },  // 6
-  { 1, 1, 1, 0, 0, 0, 0 },  // 7 
-  { 1, 1, 1, 1, 1, 1, 1 },  // 8
-  { 1, 1, 1, 0, 0, 1, 1 }   // 9
-}; 
-
-const byte Target[PatternCount][OutputNodes] = {
-  { 0, 0, 0, 0 },  
-  { 0, 0, 0, 1 }, 
-  { 0, 0, 1, 0 }, 
-  { 0, 0, 1, 1 }, 
-  { 0, 1, 0, 0 }, 
-  { 0, 1, 0, 1 }, 
-  { 0, 1, 1, 0 }, 
-  { 0, 1, 1, 1 }, 
-  { 1, 0, 0, 0 }, 
-  { 1, 0, 0, 1 } 
-};
-
-/******************************************************************
- * End Network Configuration
- ******************************************************************/
-
-
-int i, j, p, q, r;
-int ReportEvery1000;
-int RandomizedIndex[PatternCount];
-long  TrainingCycle;
-float Rando;
-float Error;
-float Accum;
-
-
-float Hidden[HiddenNodes];
-float Output[OutputNodes];
-float HiddenWeights[InputNodes+1][HiddenNodes];
-float OutputWeights[HiddenNodes+1][OutputNodes];
-float HiddenDelta[HiddenNodes];
-float OutputDelta[OutputNodes];
-float ChangeHiddenWeights[InputNodes+1][HiddenNodes];
-float ChangeOutputWeights[HiddenNodes+1][OutputNodes];
-
-void setup(){
-  Serial.begin(9600);
-  randomSeed(analogRead(3));
-  ReportEvery1000 = 1;
-  for( p = 0 ; p < PatternCount ; p++ ) {    
-    RandomizedIndex[p] = p ;
-  }
-}  
-
-void loop (){
-
-
-/******************************************************************
-* Initialize HiddenWeights and ChangeHiddenWeights 
-******************************************************************/
-
-  for( i = 0 ; i < HiddenNodes ; i++ ) {    
-    for( j = 0 ; j <= InputNodes ; j++ ) { 
-      ChangeHiddenWeights[j][i] = 0.0 ;
-      Rando = float(random(100))/100;
-      HiddenWeights[j][i] = 2.0 * ( Rando - 0.5 ) * InitialWeightMax ;
-    }
-  }
-/******************************************************************
-* Initialize OutputWeights and ChangeOutputWeights
-******************************************************************/
-
-  for( i = 0 ; i < OutputNodes ; i ++ ) {    
-    for( j = 0 ; j <= HiddenNodes ; j++ ) {
-      ChangeOutputWeights[j][i] = 0.0 ;  
-      Rando = float(random(100))/100;        
-      OutputWeights[j][i] = 2.0 * ( Rando - 0.5 ) * InitialWeightMax ;
-    }
-  }
-  Serial.println("Initial/Untrained Outputs: ");
-  toTerminal();
-/******************************************************************
-* Begin training 
-******************************************************************/
-
-  for( TrainingCycle = 1 ; TrainingCycle < 2147483647 ; TrainingCycle++) {    
-
-/******************************************************************
-* Randomize order of training patterns
-******************************************************************/
-
-    for( p = 0 ; p < PatternCount ; p++) {
-      q = random(PatternCount);
-      r = RandomizedIndex[p] ; 
-      RandomizedIndex[p] = RandomizedIndex[q] ; 
-      RandomizedIndex[q] = r ;
-    }
-    Error = 0.0 ;
-/******************************************************************
-* Cycle through each training pattern in the randomized order
-******************************************************************/
-    for( q = 0 ; q < PatternCount ; q++ ) {    
-      p = RandomizedIndex[q];
-
-/******************************************************************
-* Compute hidden layer activations
-******************************************************************/
-
-      for( i = 0 ; i < HiddenNodes ; i++ ) {    
-        Accum = HiddenWeights[InputNodes][i] ;
-        for( j = 0 ; j < InputNodes ; j++ ) {
-          Accum += Input[p][j] * HiddenWeights[j][i] ;
-        }
-        Hidden[i] = 1.0/(1.0 + exp(-Accum)) ;
-      }
-
-/******************************************************************
-* Compute output layer activations and calculate errors
-******************************************************************/
-
-      for( i = 0 ; i < OutputNodes ; i++ ) {    
-        Accum = OutputWeights[HiddenNodes][i] ;
-        for( j = 0 ; j < HiddenNodes ; j++ ) {
-          Accum += Hidden[j] * OutputWeights[j][i] ;
-        }
-        Output[i] = 1.0/(1.0 + exp(-Accum)) ;   
-        OutputDelta[i] = (Target[p][i] - Output[i]) * Output[i] * (1.0 - Output[i]) ;   
-        Error += 0.5 * (Target[p][i] - Output[i]) * (Target[p][i] - Output[i]) ;
-      }
-
-/******************************************************************
-* Backpropagate errors to hidden layer
-******************************************************************/
-
-      for( i = 0 ; i < HiddenNodes ; i++ ) {    
-        Accum = 0.0 ;
-        for( j = 0 ; j < OutputNodes ; j++ ) {
-          Accum += OutputWeights[i][j] * OutputDelta[j] ;
-        }
-        HiddenDelta[i] = Accum * Hidden[i] * (1.0 - Hidden[i]) ;
-      }
-
-
-/******************************************************************
-* Update Inner-->Hidden Weights
-******************************************************************/
-
-
-      for( i = 0 ; i < HiddenNodes ; i++ ) {     
-        ChangeHiddenWeights[InputNodes][i] = LearningRate * HiddenDelta[i] + Momentum * ChangeHiddenWeights[InputNodes][i] ;
-        HiddenWeights[InputNodes][i] += ChangeHiddenWeights[InputNodes][i] ;
-        for( j = 0 ; j < InputNodes ; j++ ) { 
-          ChangeHiddenWeights[j][i] = LearningRate * Input[p][j] * HiddenDelta[i] + Momentum * ChangeHiddenWeights[j][i];
-          HiddenWeights[j][i] += ChangeHiddenWeights[j][i] ;
-        }
-      }
-
-/******************************************************************
-* Update Hidden-->Output Weights
-******************************************************************/
-
-      for( i = 0 ; i < OutputNodes ; i ++ ) {    
-        ChangeOutputWeights[HiddenNodes][i] = LearningRate * OutputDelta[i] + Momentum * ChangeOutputWeights[HiddenNodes][i] ;
-        OutputWeights[HiddenNodes][i] += ChangeOutputWeights[HiddenNodes][i] ;
-        for( j = 0 ; j < HiddenNodes ; j++ ) {
-          ChangeOutputWeights[j][i] = LearningRate * Hidden[j] * OutputDelta[i] + Momentum * ChangeOutputWeights[j][i] ;
-          OutputWeights[j][i] += ChangeOutputWeights[j][i] ;
-        }
-      }
-    }
-
-/******************************************************************
-* Every 1000 cycles send data to terminal for display
-******************************************************************/
-    ReportEvery1000 = ReportEvery1000 - 1;
-    if (ReportEvery1000 == 0)
-    {
-      Serial.println(); 
-      Serial.println(); 
-      Serial.print ("TrainingCycle: ");
-      Serial.print (TrainingCycle);
-      Serial.print ("  Error = ");
-      Serial.println (Error, 5);
-
-      toTerminal();
-
-      if (TrainingCycle==1)
-      {
-        ReportEvery1000 = 999;
-      }
-      else
-      {
-        ReportEvery1000 = 1000;
-      }
-    }    
-
-
-/******************************************************************
-* If error rate is less than pre-determined threshold then end
-******************************************************************/
-
-    if( Error < Success ) break ;  
-  }
-  Serial.println ();
-  Serial.println(); 
-  Serial.print ("TrainingCycle: ");
-  Serial.print (TrainingCycle);
-  Serial.print ("  Error = ");
-  Serial.println (Error, 5);
-
-  toTerminal();
-
-  Serial.println ();  
-  Serial.println ();
-  Serial.println ("Training Set Solved! ");
-  Serial.println ("--------"); 
-  Serial.println ();
-  Serial.println ();  
-  ReportEvery1000 = 1;
-}
-
-void toTerminal()
-{
-
-  for( p = 0 ; p < PatternCount ; p++ ) { 
-    Serial.println(); 
-    Serial.print ("  Training Pattern: ");
-    Serial.println (p);      
-    Serial.print ("  Input ");
-    for( i = 0 ; i < InputNodes ; i++ ) {
-      Serial.print (Input[p][i], DEC);
-      Serial.print (" ");
-    }
-    Serial.print ("  Target ");
-    for( i = 0 ; i < OutputNodes ; i++ ) {
-      Serial.print (Target[p][i], DEC);
-      Serial.print (" ");
-    }
-/******************************************************************
-* Compute hidden layer activations
-******************************************************************/
-
-    for( i = 0 ; i < HiddenNodes ; i++ ) {    
-      Accum = HiddenWeights[InputNodes][i] ;
-      for( j = 0 ; j < InputNodes ; j++ ) {
-        Accum += Input[p][j] * HiddenWeights[j][i] ;
-      }
-      Hidden[i] = 1.0/(1.0 + exp(-Accum)) ;
-    }
-
-/******************************************************************
-* Compute output layer activations and calculate errors
-******************************************************************/
-
-    for( i = 0 ; i < OutputNodes ; i++ ) {    
-      Accum = OutputWeights[HiddenNodes][i] ;
-      for( j = 0 ; j < HiddenNodes ; j++ ) {
-        Accum += Hidden[j] * OutputWeights[j][i] ;
-      }
-      Output[i] = 1.0/(1.0 + exp(-Accum)) ; 
-    }
-    Serial.print ("  Output ");
-    for( i = 0 ; i < OutputNodes ; i++ ) {       
-      Serial.print (Output[i], 5);
-      Serial.print (" ");
-    }
-  }
-
-
-}
-
-/*
 #include <Arduino.h>
 #include <Ultrasonic.h> // lib link - https://github.com/JRodrigoTech/Ultrasonic-HC-SR04
 #include <pt.h>   // include protothread library - https://roboticsbackend.com/arduino-protothreads-tutorial/
+#include <pb_encode.h> // nanopb lib from pio - https://platformio.org/lib/show/431/Nanopb
+#include <pb_decode.h>
+#include "simple.pb.h"
+
 
 //Wifi Module uses x pins, adding later
 
@@ -308,15 +12,15 @@ void toTerminal()
 static struct pt pt1, pt2;
 
 // Create ultrasonic sensor objects to use in logic
-Ultrasonic us1(A0,A1); // (Trig PIN,Echo PIN)
-Ultrasonic us2(A2,A3); // pinout, pinin
-Ultrasonic us3(A4,A5);
+Ultrasonic us1(A0,A1); // (Trig PIN,Echo PIN) pinout, pinin
+Ultrasonic us2(A2,A3); // on the left
+Ultrasonic us3(A4,A5); // on teh right
 
 // setup motor pin variables
-int m4 = 13;
-int m3 = 12;
-int m2 = 11;
-int m1 = 10;
+int m4 = 11;
+int m3 = 10;
+int m2 = 6;
+int m1 = 5;
 
 // start serial monitor if needed (debugging), and
 // set pinout
@@ -345,24 +49,16 @@ void setup() {
 // the infinite loop to run on the arduino
 void loop()
 {
-  
-  
-  Serial.print("US-1 " + us1.Ranging(CM)); // CM or INC
-  Serial.println(" cm" );
-  delay(10);
-  Serial.print("US-2 " + us2.Ranging(CM)); // CM or INC
-  Serial.println(" cm" );
-  delay(10);
-  Serial.print("US-3 " + us3.Ranging(CM)); // CM or INC
-  Serial.println(" cm" );
-  delay(10);
-  
   // schedule threads indefinitely
   prot1(&pt1);
   prot2(&pt2);
 }
 
 
+/* This thread determines if the robot is
+   setup to move forward, if not, then start
+   moving the robot forward 
+*/
 static int prot1(struct pt *pt) {
   PT_BEGIN(pt);
   if (digitalRead(m1) != 0 || digitalRead(m2) != 1 || digitalRead(m3) != 0 || digitalRead(m4) != 1) {
@@ -371,6 +67,19 @@ static int prot1(struct pt *pt) {
   PT_END(pt);
 }
 
+/* This thread gathers and interprets data
+   from the ultrasonic sensors and makes
+   decisions for movement based on that data.
+   
+   Specifically, if an object is measured to 
+   be at or less than 10cm away, then the robot
+   will react in accordance to the logic below.
+   
+   SideNote: 
+   us1 is in front
+   us2 is facing left, so turn right
+   us3 is facing right, so turn left
+*/
 static int prot2(struct pt *pt) {
   PT_BEGIN(pt);
   if (us1.Ranging(CM) <= 10) {
@@ -389,13 +98,15 @@ static int prot2(struct pt *pt) {
   PT_END(pt);
 }
 
+  // Allocate voltage to motor pins so that
+  // the robot moves forward
 void forw() {
   digitalWrite(m1, LOW);// right wheel forward
   digitalWrite(m2, HIGH);//
   digitalWrite(m3, LOW);// left wheel forward
   digitalWrite(m4, HIGH);//
 }
-
+  // Allocate voltage to move backwards
 void back() {
   digitalWrite(m3, HIGH);// left wheel back
   digitalWrite(m4, LOW);//
@@ -403,18 +114,17 @@ void back() {
   digitalWrite(m2, LOW);//
 
 }
-
+  // Turn right
 void rturn() {
   digitalWrite(m1, HIGH);// right wheel back ward
   digitalWrite(m2, LOW);//
   digitalWrite(m3, LOW);// left wheel forward
   digitalWrite(m4, HIGH);//
 }
-
+  // Turn left
 void lturn() {
   digitalWrite(m3, HIGH);// left wheel back
   digitalWrite(m4, LOW);//
   digitalWrite(m1, LOW);// right wheel forward
   digitalWrite(m2, HIGH);//  
 }
-*/
